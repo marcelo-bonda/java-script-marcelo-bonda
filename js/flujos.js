fe23e303b9b8b9445ee6c701961b700c4dfabede
marcelo bonda <marcelo.pf16@gmail.com>
2022-10-17 12:25:41 -0300

    pre-entrega1-Bonda

---
+++ js/flujos.js
@@ -0,0 +1,84 @@
+//punto 1
+
+/*let edad = parseFloat(prompt('cual es tu edad?'))
+
+if (edad <= '17'){
+    alert('usted es menor de edad')
+}
+else {
+    alert ('usted es mayor de edad')
+}
+*/
+//punto 2
+
+/*let contrasena = prompt('escriba su contraseña para poder ingresar')
+
+if (contrasena==='messi el mejor'){
+    alert('contraseña correcta, bienvenido!')
+}
+else if(contrasena!= 'messi el mejor') {
+    contrasena = prompt('contraseña incorrecta, vuelva a intentarlo')
+    
+}
+*/
+//punto 3 
+
+/*let dividendo = parseFloat(prompt('ingrese un numero'))
+
+let divisor = parseFloat(prompt('ingrese un numero mas para divirlo con el anterior'))
+
+if(divisor===0){
+    divisor = parseFloat(prompt('error, ingrese un numero valido para divirlo con el anterior'))
+}
+
+let division= dividendo / divisor
+
+alert('el resultado es '+ division)
+
+*/
+//punto 4
+
+let numero = prompt('ingrese un numero ')
+
+if(numero %2===0){
+    alert('el numero '+numero +' es par')
+}
+
+else {
+    alert('el numero '+numero +' es impar')
+}
+
+
+//punto 5
+
+/*let edad = prompt('ingrese su edad')
+
+let ingresos = prompt ('agregue sus ingresos')
+
+if((edad >=16)&&(ingresos >=1000)){
+    alert('usted debe tributar')
+}
+else{
+    alert('usted no debe tributar')
+}
+*/
+
+//punto 6
+
+/*let nombre = prompt('ingrese su nombre')
+
+let sexo = prompt ('ingrese su sexo')
+
+if ((sexo==='mujer')&&(nombre<'m')||(sexo==='hombre')&&(nombre>'n')){
+    alert('usted es del grupo A')
+}
+else{
+    alert('usted es del grupo B')
+}
+*/
+
+//punto 7
+
+let renta = parseFloat(prompt('declare el valor de su renta'))
+
+
---
+++ js/funciones.js
@@ -0,0 +1,79 @@
+/*function saludar (){
+    alert('hola mundo')
+}
+
+saludar();
+
+*/
+
+//punto 2
+
+/*let saludo = prompt('cual es tu nombre?')
+
+function saludar(){
+    alert ('hola '+saludo+', es un placer')
+}
+
+saludar();
+
+*/
+
+//punto 3 
+
+/*let numero = Number(prompt('escriba un numero'))
+let producto = 1
+
+    for(let i=numero;i>0;i--){
+        producto*=i;
+    }
+
+    alert(producto)
+
+    */
+
+//punto 4
+
+/*
+let factura = Number(prompt('indique el valor de la factura'))
+
+let iva = Number(prompt('indique el valor del iva'))
+
+
+
+function final (primerNumero,segundoNumero){
+return primerNumero +(primerNumero*segundoNumero/100)
+
+}
+
+let resultado = final(factura,iva);
+
+console.log(resultado)
+*/
+
+//punto 5
+
+
+
+
+
+
+
+//punto 6
+
+let primerNumero= Number(prompt('escriba un primer numero'))
+
+let segundoNumero=Number(prompt('escriba un segundo numero'))
+
+let tercerNumero = Number(prompt('escriba un tercer numero'))
+
+function promedio (primero,segundo,tercero){
+    return (primero+segundo+tercero)/3
+}
+let resultado = promedio(primerNumero,segundoNumero,tercerNumero);
+
+console.log(resultado)
+
+
+
+
+
